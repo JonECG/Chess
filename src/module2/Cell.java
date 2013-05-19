81666e49d321328587b2e36bb3649ecd1dd2732c
Jonathan Pearl <jonathantpearl@gmail.com>
2013-05-19 12:36:01 +0000

    All basic moves implemented. -Pieces can't capture pieces of their own color -Pawn can only move two spaces when it is their first move -Sliding pieces (Bishop, Rook, Queen) now are blocked if pieces are in the way to their destination
    git-svn-id: https://svn.neumont.edu:8443/svn/jpearl_chess@5 86f9d0df-9005-f54b-96e6-c011f00df492

--- src/module2/Cell.java
+++ src/module2/Cell.java
@@ -4,7 +4,10 @@
  */
 package module2;
 
+import java.util.ArrayList;
+
 import module2.Move.MoveCase;
+import module2.Move.MoveStyle;
 import module2.Move.MoveType;
 
 public class Cell
@@ -25,37 +28,19 @@ public class Cell
 		if ( piece != null )
 		{
 			MoveSet potentialMoves = piece.getMoves();
-			if (potentialMoves.containsMove( suggestion, piece.getDirection() ))
+			ArrayList<Move> applicableMoves = potentialMoves.matchMoves( suggestion, piece.getDirection() );
+			//if (potentialMoves.containsMove( suggestion, piece.getDirection() ))
+			if (applicableMoves.size() > 0)
 			{
 				Location adjustedLocation = location.addMove( suggestion );
 				Cell adjustedCell = board.getCell( adjustedLocation );
-				if( suggestion.getType() == MoveType.CAPTURE )
+				
+				boolean moveFound = false;
+				for( Move move : applicableMoves )
 				{
-					if (adjustedCell.hasPiece())
+					if( !moveFound )
 					{
-						if (adjustedCell.getPiece().getColor() != piece.getColor())
-						{
-							placePieceAt( adjustedCell );
-						}
-						else
-						{
-							System.out.println( "<<ILLEGAL CAPTURE -- OTHER PIECE BELONGS TO CAPTURING PLAYER>>" );
-						}
-					}
-					else
-					{
-						System.out.println( "<<ILLEGAL CAPTURE -- NO PIECE TO CAPTURE>>" );
-					}
-				}
-				else if( suggestion.getType() == MoveType.MOVE )
-				{
-					if (!adjustedCell.hasPiece())
-					{
-						placePieceAt( adjustedCell );
-					}
-					else
-					{
-						System.out.println( "<<ILLEGAL MOVE -- NEW LOCATION IS OCCUPIED>>" );
+						moveFound = considerMove( suggestion, move, adjustedCell );
 					}
 				}
 			}
@@ -68,6 +53,59 @@ public class Cell
 		{
 			System.out.println( "<<ILLEGAL MOVE -- THERE IS NO PIECE TO MOVE>>" );
 		}
+	}
+	
+	private boolean considerMove( Move potentialMove, Move referenceMove, Cell toCell )
+	{
+		boolean moveWasMade = false;
+		
+		if( evaluateSpecialCases( potentialMove ) )
+		{
+			if ( referenceMove.getStyle() != MoveStyle.SLIDE || isSlideUnblocked( potentialMove, referenceMove ) )
+			{
+				if( potentialMove.getType() == MoveType.CAPTURE )
+				{
+					if (toCell.hasPiece())
+					{
+						if (toCell.getPiece().getColor() != piece.getColor())
+						{
+							placePieceAt( toCell );
+							moveWasMade = true;
+						}
+						else
+						{
+							System.out.println( "<<ILLEGAL CAPTURE -- OTHER PIECE BELONGS TO CAPTURING PLAYER>>" );
+						}
+					}
+					else
+					{
+						System.out.println( "<<ILLEGAL CAPTURE -- NO PIECE TO CAPTURE>>" );
+					}
+				}
+				else if( potentialMove.getType() == MoveType.MOVE )
+				{
+					if (!toCell.hasPiece())
+					{
+						placePieceAt( toCell );
+						moveWasMade = true;
+					}
+					else
+					{
+						System.out.println( "<<ILLEGAL MOVE -- NEW LOCATION IS OCCUPIED>>" );
+					}
+				}
+			}
+			else
+			{
+				System.out.println( "<<ILLEGAL MOVE -- SLIDE MOVEMENT IS BLOCKED>>" );
+			}
+		}
+		else
+		{
+			System.out.println( "<<ILLEGAL MOVE -- MOVE'S SPECIAL CASE WAS NOT SATISFIED>>" );
+		}
+		
+		return moveWasMade;
 	}
 	
 	private boolean evaluateSpecialCases( Move evaluating )
@@ -95,8 +133,38 @@ public class Cell
 		return result;
 	}
 	
+	private boolean isSlideUnblocked( Move destination, Move slideReference )
+	{
+		boolean result = true;
+		
+		Location simulate = location;
+		
+		int maxNumberOfTilesToCheck;
+		
+		if ( slideReference.getDeltaX() == 0 )
+		{
+			maxNumberOfTilesToCheck = destination.getDeltaY()/slideReference.getDeltaY();
+		}
+		else
+		{
+			maxNumberOfTilesToCheck = destination.getDeltaX()/slideReference.getDeltaX();
+		}
+		
+		for( int i = 1; i < maxNumberOfTilesToCheck; i++ )
+		{
+			if (result)
+			{
+				simulate = simulate.addMove( slideReference );
+				result = ( !board.getCell( simulate ).hasPiece() );
+			}
+		}
+		
+		return result;
+	}
+	
 	private void placePieceAt( Cell adjustedCell )
 	{
+		piece.move();
 		if ( adjustedCell.hasPiece() )
 		{
 			adjustedCell.takePiece();
